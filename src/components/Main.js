c308810328e11c0ec4b92cb9fb49828754dd3d24
ksopel <katarzyna.sopel@outlook.com>
2022-06-29 10:34:50 +0200

    parallax done

---
+++ src/components/Main.js
@@ -0,0 +1,53 @@
+import "../scss/main.scss"
+
+
+
+function Main() {
+
+    return (
+        <>
+            <div className="img1">
+                <div className="text1">
+                    <span>parallax</span>
+                </div>
+            </div>
+            <section className="section1">
+                <h2>first section</h2>
+                <p>Hic distinctio mollitia qui exercitationem aspernatur eum nihil dolores. Et doloremque quia vel iusto dolorem est fuga magni qui quisquam repudiandae ut nemo molestias. Qui aspernatur repellendus est recusandae doloremque qui earum tempora aut veniam tempore est illum sint. Qui dolorem facilis id distinctio Quis et quasi sint qui galisum architecto qui voluptatem accusantium aut dicta voluptas ut maiores optio.
+
+                    Sit saepe voluptas eos eaque quas vel suscipit accusamus et architecto optio et molestiae rerum qui necessitatibus cupiditate eos nulla quidem. Vel recusandae perferendis qui quas inventore est esse quis quo possimus quia et recusandae enim ut fuga consequuntur At voluptates iste. Ut dolores placeat ut doloribus aliquid 33 magnam rerum et voluptatem consequatur sit atque porro et sunt architecto. Ex maiores deleniti et omnis officia et pariatur tenetur sit quod quia.</p>
+            </section>
+            <div className="img2">
+                <div className="text1">
+                    <span>parallax</span>
+                </div>
+            </div>
+            <section className="section1">
+                <h2>second section</h2>
+                <p>Hic distinctio mollitia qui exercitationem aspernatur eum nihil dolores. Et doloremque quia vel iusto dolorem est fuga magni qui quisquam repudiandae ut nemo molestias. Qui aspernatur repellendus est recusandae doloremque qui earum tempora aut veniam tempore est illum sint. Qui dolorem facilis id distinctio Quis et quasi sint qui galisum architecto qui voluptatem accusantium aut dicta voluptas ut maiores optio.
+
+                    Sit saepe voluptas eos eaque quas vel suscipit accusamus et architecto optio et molestiae rerum qui necessitatibus cupiditate eos nulla quidem. Vel recusandae perferendis qui quas inventore est esse quis quo possimus quia et recusandae enim ut fuga consequuntur At voluptates iste. Ut dolores placeat ut doloribus aliquid 33 magnam rerum et voluptatem consequatur sit atque porro et sunt architecto. Ex maiores deleniti et omnis officia et pariatur tenetur sit quod quia.</p>
+            </section>
+            <div className="img3">
+                <div className="text1">
+                    <span>parallax</span>
+                </div>
+            </div>
+            <section className="section1">
+                <h2>third section</h2>
+                <p>Hic distinctio mollitia qui exercitationem aspernatur eum nihil dolores. Et doloremque quia vel iusto dolorem est fuga magni qui quisquam repudiandae ut nemo molestias. Qui aspernatur repellendus est recusandae doloremque qui earum tempora aut veniam tempore est illum sint. Qui dolorem facilis id distinctio Quis et quasi sint qui galisum architecto qui voluptatem accusantium aut dicta voluptas ut maiores optio.
+
+                    Sit saepe voluptas eos eaque quas vel suscipit accusamus et architecto optio et molestiae rerum qui necessitatibus cupiditate eos nulla quidem. Vel recusandae perferendis qui quas inventore est esse quis quo possimus quia et recusandae enim ut fuga consequuntur At voluptates iste. Ut dolores placeat ut doloribus aliquid 33 magnam rerum et voluptatem consequatur sit atque porro et sunt architecto. Ex maiores deleniti et omnis officia et pariatur tenetur sit quod quia.</p>
+            </section>
+            <div className="img1">
+                <div className="text1">
+                    <span>parallax</span>
+                </div>
+            </div>
+        </>
+
+
+    );
+}
+
+export default Main;
